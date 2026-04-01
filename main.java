2d0755b8f1d0ee6b71d3ff25aab688fe99007a96
k14321035-cmd <k14321035@gmail.com>
2026-04-01 21:16:45 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main{
+	public static void main(String[] args){
+		int num =10;
+		String name="tate";
+		System.out.println("Hello " + name);
+		for (int i=1;i<10;i++){
+			System.out.println(i * i);
+		}
+	}
+}
